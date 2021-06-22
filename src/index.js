43bd33b027685b5280ca52f454641a0575ccfb10
MusicDev33 <smccowan33@gmail.com>
2021-06-22 13:25:40 -0600

    complete rewrite with FS

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const canStart = require('./startup').checkArgs;
 
 require('dotenv-defaults').config();
 
-const { exec } = require('child-process-async');
-const execWrap = require('./exec-wrap').execWrap;
 const userblock = require('./middleware/userblock');
 const logger = require('./logger').logger;
 
@@ -33,21 +31,9 @@ app.use(cors());
 // Images can be retrieved with http://url/biotec/image/:imagename
 require('./rsc-routes')(app);
 
-
-// Pages
-
-// Default browser behaviour fix
-
-const homeDict = {
-  'darwin': '/Users',
-  'linux': '/home'
-};
-
 nunEnv.addGlobal(`BASE_URL`, process.env.NUNJUCKS_BASE_URL);
 
-const homePath = homeDict[process.platform];
-
-app.get('/:user', async (req, res, next) => {
+app.get('/:user', userblock, async (req, res, next) => {
   let totalPath = `${baseDir}/${req.params.user}`;
   console.log(totalPath);
 
@@ -99,66 +85,44 @@ app.get('/:user', async (req, res, next) => {
 
 });
 
+// TODO: Rewrite this whole thing
 app.get('/:user/:filepath*', userblock, async (req, res, next) => {
+  let totalPath = `${baseDir}/${req.params.user}/${req.params.filepath}/${req.params[0]}`.replace(/\/$/, '');
 
-  let totalPath = `${homePath}/${req.params.user}/.html/${req.params.filepath}/${req.params[0]}`;
+  const stat = await fs.lstat(totalPath);
 
-  if (req.params.user == 'rkaundal' || req.params.user == 'roussie') {
-    totalPath = `${homePath}/${req.params.user}/.psc6150/${req.params.filepath}/${req.params[0]}`;
+  if (stat.isFile()) {
+    return res.download(totalPath);
   }
 
-  if (totalPath.includes(' ')) {
-    totalPath = `'${totalPath}'`;
+  try {
+    results = await fs.readdir(totalPath);
+  } catch (e) {
+    return res.json({success: false});
   }
 
-  const fileCheck = await execWrap(`file ${totalPath}`);
+  let files = [];
+  let dirs = [];
 
-  if (fileCheck === null || fileCheck.stdout.includes('No such file or directory')) {
-    logger.error(`User tried to access path '${totalPath}' and it failed.`);
-    return res.render(__dirname + '/views/pages/dir-not-found.njk');
-  }
+  for (let file of results) {
+    const stat = await fs.lstat(`${totalPath}/${file}`);
 
-  const isFile = fileCheck.stdout.split(':')[1].trim().toLowerCase() == 'directory' ? false : true;
-
-  if (isFile) {
-    return res.download(totalPath.substring(0, totalPath.length - 1));
-  }
-
-  const { stdout, stderr } = await exec(`ls ${totalPath}`);
-
-  let contents = stdout.trim().split(/\r?\n/);
-  contents = contents.filter((item) => {
-    return item.length > 0;
-  });
-
-  const directories = [];
-  const files = [];
-
-  for (const item of contents) {
-    let file = '';
-    if (item.includes(' ')) {
-      file = `'${path.join(totalPath, item)}'`;
-    } else {
-      file = path.join(totalPath, item);
-    }
-
-    const output = await exec(`file ${file}`);
-    const isDirectory = output.stdout.split(':')[1].trim().toLowerCase();
-    if (isDirectory == 'directory') {
-      const newDirectory = {
-        name: item,
-        link: item.trim().replace(' ', '%20')
-      }
-      console.log(newDirectory.link);
-      directories.push(newDirectory);
-    } else {
-      const splitName = item.split('.');
+    if (stat.isFile()) {
+      const splitName = file.split('.');
 
       const newFile = {
-        name: item,
-        icon: FileMap.get(splitName[splitName.length - 1]) || 'fa-file-alt'
+        name: file,
+        icon: FileMap.get(splitName[splitName.length - 1].toLowerCase()) || 'fa-file-alt'
       }
       files.push(newFile);
+    } else if (stat.isDirectory()) {
+      const newDirectory = {
+        name: file,
+        link: file.trim().replace(' ', '%20')
+      }
+      dirs.push(newDirectory);
+    } else {
+      console.log('Error');
     }
   }
 
@@ -171,15 +135,12 @@ app.get('/:user/:filepath*', userblock, async (req, res, next) => {
     id: req.params.filepath,
     path: req.params,
     user: req.params.user,
-    dirs: directories,
+    dirs: dirs,
     files: files,
     currentRoute: req.originalUrl.replace(/\/$/, ''),
     backDir: '',
     prevRoute
   }
-
-  // res.header("Content-Type",'application/json');
-  // res.send(JSON.stringify(payload, null, 4));
 
   logger.info(`User successfully accessed path '${totalPath}'`);
 
